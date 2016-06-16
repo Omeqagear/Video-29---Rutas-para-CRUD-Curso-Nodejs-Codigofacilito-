a8c344bdebc57bad8a8ce39cd3179ad8c3d41712
Andres Lopez <andres.lopez.hernandez.91@gmail.com>
2016-06-16 07:46:14 -0430

    Video 29 - Rutas para CRUD

---
+++ routes_app.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var router = express.Router();
+var Imagen = require("./models/imagenes").Imagen;
+router.get("/",function (req,res) {
+    res.render("app/home");
+});
+router.get("/imagenes/new",function (req,res) {
+    res.render("app/imagenes/new");
+});
+router.get("/imagenes/:id/edit",function (req,res) {
+
+});
+router.route("/imagenes/:id")
+    .get(function (req,res) {
+        Imagen.findById(req.params.id,function (err,imagen) {
+            res.render("app/imagenes/show",{
+                imagen:imagen
+            });
+        })
+    })
+    .put(function (req,res) {
+
+    })
+    .delete(function (req,res) {
+
+    });
+router.route("/imagenes")
+    .get(function (req,res) {
+
+    })
+    .post(function (req,res) {
+        var data = {
+            title: req.body.title
+        }
+        var imagen = new Imagen(data);
+        imagen.save(function (err) {
+            if(!err){
+                res.redirect("/app/imagenes/"+imagen._id);
+            }else{
+                res.send(String(err));
+            }
+        });
+    });
+
+module.exports = router;
